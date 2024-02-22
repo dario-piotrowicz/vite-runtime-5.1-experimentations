ceb6ee78001b92088d0915e88e21258b014e304b
Dario Piotrowicz <dario@cloudflare.com>
2024-02-22 23:10:32 +0000

    add note about nodejs entrypoint assumption

--- packages/vite-runtime-node-plugin/src/client/index.ts
+++ packages/vite-runtime-node-plugin/src/client/index.ts
@@ -28,6 +28,10 @@ export async function dispatchRequestImplementation(req: Request) {
   runtime.moduleCache.clear();
 
   const entrypointModule = await runtime.executeUrl(__ENTRYPOINT__);
+  // Note: here we make the assumption that an entrypoint for the Nodejs runtime
+  //       has a default export with a `fetch` method that takes a request and returns
+  //       a response (akin to what happens in workerd).
+  //       This is likely not a great assumption and something we'll need to address.
   const fetch = entrypointModule.default.fetch;
   return fetch(req);
 }
