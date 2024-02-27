aec164e6c43ceab081ad62d4438eab93060893ff
Dario Piotrowicz <dario@cloudflare.com>
2024-02-27 18:57:57 +0000

    make server/time also log on SSR HMR

--- examples/basic-handler/src/server/time.ts
+++ examples/basic-handler/src/server/time.ts
@@ -1,3 +1,13 @@
+if (import.meta.hot) {
+  import.meta.hot.accept(() => {
+    // It would be very convenient here if we'd have a simple
+    // clear way to communicate with the browser and instruct
+    // it to do a full page reload or whatever else that might
+    // be appropriate for handling the server changes
+    console.log('__server/time has changed (SSR HMR is working!)__');
+  });
+}
+
 export function getCurrentTimeText(): string {
   return new Date().toLocaleString();
 }
