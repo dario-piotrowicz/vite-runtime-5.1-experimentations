0cd451c8cd0ea44d5e3f3222ae57855ca3700667
Dario Piotrowicz <dario@cloudflare.com>
2024-03-11 16:31:27 +0000

    move workerd vite configs inside remix plugin

--- examples/remix-app-workerd/vite.config.ts
+++ examples/remix-app-workerd/vite.config.ts
@@ -4,29 +4,6 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 import { viteRuntimeWorkerd } from 'vite-runtime-workerd-plugin';
 
 export default defineConfig({
-  ssr: {
-    noExternal: true,
-    target: 'webworker',
-    resolve: {
-      externalConditions: ['workerd', 'worker'],
-    },
-    optimizeDeps: {
-      // Add CJS dependencies that break code in workerd
-      // with errors like "require/module/exports is not defined":
-      include: [
-        // React deps:
-        'react',
-        'react/jsx-runtime',
-        'react/jsx-dev-runtime',
-        'react-dom',
-        'react-dom/server',
-        // Remix deps:
-        'set-cookie-parser',
-        'cookie',
-        '@remix-run/react/dist/esm/index.js',
-      ],
-    },
-  },
   plugins: [
     viteRuntimeWorkerd(),
     remix({
